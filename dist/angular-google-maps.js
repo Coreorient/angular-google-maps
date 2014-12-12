a1b93353aa5af7d591c70182039f6e038eb77274
Gerard Bosch Creus <gerard.bosch.i.creus@gmail.com>
2014-12-12 13:26:36 +0200

    Bump version

--- dist/angular-google-maps.js
+++ dist/angular-google-maps.js
@@ -1,4 +1,4 @@
-/*! angular-google-maps 2.0.11 2014-12-11
+/*! angular-google-maps 2.0.12 2014-12-12
  *  AngularJS directives for Google Maps
  *  git: https://github.com/angular-ui/angular-google-maps.git
  */
